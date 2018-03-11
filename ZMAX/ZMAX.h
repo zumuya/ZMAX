b404e75006c24366ce16fb5af44b9220bdaedcfc
zumuya <zumuya@gmail.com>
2018-03-11 11:09:21 +0900

    Initial commit

---
+++ ZMAX/ZMAX.h
@@ -0,0 +1,36 @@
+/*
+ ZMAX.h
+
+ Created by zumuya on 2018/03/10.
+
+ Copyright 2018 zumuya
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial
+ portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ APARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZMAX.
+FOUNDATION_EXPORT double ZMAXVersionNumber;
+
+//! Project version string for ZMAX.
+FOUNDATION_EXPORT const unsigned char ZMAXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZMAX/PublicHeader.h>
+
+
