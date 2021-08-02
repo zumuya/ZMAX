47876fc72264cef68fa8d1adbb2782aa3970d1b5
soundflix <felixrost@yahoo.de>
2021-08-03 00:30:27 +0200

    updated Package.swift fixed folder structure

--- Package.swift
+++ Package.swift
@@ -4,9 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "ZMAX",
-    platforms: [
-        .macOS(.v10_14_4),    //.v10_14_4 - .v10_15
+    products: [
+        .library(
+            name: "ZMAX",
+            targets: ["ZMAX"]),
     ],
     targets: [
-        .target(name: "ZMAXLib")]
+        .target(
+            name: "ZMAX",
+            path: "Sources"),
+    ]
 )
