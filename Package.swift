67187a07c70bc74a305c98fff6141d8650fc44cf
soundflix <80773469+soundflix@users.noreply.github.com>
2021-08-03 12:00:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "ZMAX",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "ZMAX",
