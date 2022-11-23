b1d12226b510a95634e34267caf89bcb945fce91
soundflix <80773469+soundflix@users.noreply.github.com>
2022-11-23 01:30:40 +0100

    changed toValue / from<T> to public

--- ZMAX/Sources/Extensions.swift
+++ ZMAX/Sources/Extensions.swift
@@ -208,13 +208,13 @@ extension AXUIElement {
 // for conversion AXValue (wrapper) <> wrapped value
 // from: https://github.com/keith/ModMove/blob/main/ModMove/AXValue%2BHelper.swift
 extension AXValue {
-    func toValue<T>() -> T? {
+    public func toValue<T>() -> T? {
         let pointer = UnsafeMutablePointer<T>.allocate(capacity: 1)
         let success = AXValueGetValue(self, AXValueGetType(self), pointer)
         return success ? pointer.pointee : nil
     }
 
-    static func from<T>(value: T, type: AXValueType) -> AXValue? {
+    public static func from<T>(value: T, type: AXValueType) -> AXValue? {
         let pointer = UnsafeMutablePointer<T>.allocate(capacity: 1)
         pointer.pointee = value
         return AXValueCreate(type, pointer)
